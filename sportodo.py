6d07c2fcc1e2527c13cc6a6fcb481c8b38b338c4
Gareth Spor <gspor@lytro.com>
2018-05-19 20:44:20 -0700

    find and load

--- sportodo.py
+++ sportodo.py
@@ -1,16 +1,39 @@
 ''' A simple commandline todo list utility
 '''
+from __future__ import print_function
+
 import argparse
 import json
 import os
 import sys
 
-TODO_FILENAME = '.' + os.path.splitext(__file__)[0] + '.json'
+TODO_FILENAME = '.sportodo.json'
+
+
+def find_list(path):
+    while path:
+        test_path = os.path.join(path, TODO_FILENAME)
+        if os.path.exists(test_path):
+            return test_path
+        if path == os.path.sep:
+            return None
+        path = os.path.split(path)[0]
+
+
+def find_and_load_list(path):
+    list_path = find_list(path)
+    if list_path is None:
+        return None
+    with open(list_path, 'r') as fileobj:
+        list = json.load(fileobj)
+    return list
+
 
 def init(args):
     """ initialize a todo file in the current dir
     """
     cwd = os.getcwd()
+    print(cwd)
     list_path = os.path.join(cwd, TODO_FILENAME)
     if os.path.exists(list_path):
         print('This directory: {} is already intialized'.format(cwd))
@@ -22,12 +45,24 @@ def init(args):
     return 0
 
 
+def list(args):
+    list = find_and_load_list(os.getcwd())
+    print(list)
+
+
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     subparsers = parser.add_subparsers(dest='cmd')
 
-    init_subparser = subparsers.add_parser('init')
+    init_subparser = subparsers.add_parser(
+        'init',
+        help='initialize a todo list in current working dir')
     init_subparser.set_defaults(func=init)
+
+    init_subparser = subparsers.add_parser(
+        'list',
+        help='enumerate the todo list items')
+    init_subparser.set_defaults(func=list)
 
     args = parser.parse_args()
     return args.func(args)
